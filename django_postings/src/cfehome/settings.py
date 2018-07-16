8a48a7992c16ac3365e75327c52180da75f0e262
andrezb5 <azarta@clickdelivery.com>
2018-07-16 17:54:15 -0500

    sample database

--- django_postings/src/cfehome/settings.py
+++ django_postings/src/cfehome/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'cfehome.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sample_database',
+        'USER': 'sample_user',
+        'PASSWORD': 'sample_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

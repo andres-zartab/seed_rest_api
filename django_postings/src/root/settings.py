fb84962b265affff923e39c6b68c21561419ac83
andrezb5 <azarta@clickdelivery.com>
2018-07-25 12:01:50 -0500

    django project renamed to root, plants initialized

--- django_postings/src/root/settings.py
+++ django_postings/src/root/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'cfehome.urls'
+ROOT_URLCONF = 'root.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cfehome.wsgi.application'
+WSGI_APPLICATION = 'root.wsgi.application'
 
 
 # Database
